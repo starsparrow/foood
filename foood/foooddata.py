67ebc21b6f39812cc183df4d0234808ec72a85f8
starsparrow <nik.spurgetis@gmail.com>
2017-02-27 19:20:54 -0500

    FINALLY got the module import for foooddata working

--- foood/foooddata.py
+++ foood/foooddata.py
@@ -1,1 +1,17 @@
-meals = ['a','b','c']
+meals = [
+    'Pizza',
+    'Burgers',
+    'Sushi',
+    'Tacos',
+    'Pork Tenderloin',
+    'Roast Chicken',
+    'Korean Pork',
+    'Tofu and Eggplant',
+    'French F#*@in Chicken',
+    'Pasta Salad',
+    'Pasta with Niko Sauce and Meatballs',
+    'Braised Atlas Shanks',
+    'Angel Hair Pasta with Zucchini',
+    'Kebabs',
+    'Paninis'
+]
