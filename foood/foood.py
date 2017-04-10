a529827ae9139ce7095d714c71362ca991fda531
Nik Spurgetis <nspurget@iu.edu>
2017-04-09 22:49:40 -0400

    Added 5-day planner page

--- foood/foood.py
+++ foood/foood.py
@@ -11,11 +11,24 @@ app.config.update(dict(
 
 from foood import foooddata
 
+def random_meals(n):
+    '''Returns list of n random choices (no repeats) from list of all meals'''
+    meals = []
+    while len(meals) < (n + 1):
+        random_meal = choice(foooddata.meals)
+        if random_meal not in meals:
+            meals.append(random_meal)
+    return meals
+
 @app.route('/')
 def show_meal():
-    return render_template('meal.html', meal=choice(foooddata.meals))
+    return render_template('meal.html', meal=random_meals(1)[0])
 
 @app.route('/all')
 def show_all():
     return render_template('all.html', meals=foooddata.meals)
+
+@app.route('/planner')
+def show_plan():
+    return render_template('planner.html', meals=random_meals(5))
 
